eb555bad11bcbc6e12aa2e4df51cc33018d0e794
andrew-1234 <andrew.schwenke@gmail.com>
2022-09-22 00:43:52 +1000

    robotgo added
    adds key press capabilities

--- research-hotkeys.go
+++ research-hotkeys.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 
+	"github.com/go-vgo/robotgo"
 	hook "github.com/robotn/gohook"
 	"github.com/toqueteos/webbrowser"
+	"github.com/zyedidia/clipper"
 )
 
 func main() {
@@ -20,10 +22,40 @@ func scholar() {
 
 	hook.Register(hook.KeyDown, []string{"w"}, func(sch hook.Event) {
 		fmt.Println("w")
-
+		getstring()
+		pastestring()
 		webbrowser.Open("http://golang.org")
+		webbrowser.Open("https://scholar.google.com.au/scholar?hl=en&as_sdt=0%2C5&q=text%20goes%20here&btnG=")
+		web1 := "https://scholar.google.com.au/scholar?hl=en&as_sdt=0%2C5&q="
+		web2 := "&btnG="
+		web3 := web1 + web2
 	})
 
 	start := hook.Start()
 	<-hook.Process(start)
 }
+
+func getstring() {
+	//robotgo.TypeStr("Hello")
+	robotgo.KeyTap("c", "cmd")
+
+}
+func pastestring() {
+	clip, _ := clipper.GetClipboard(clipper.Clipboards...)
+
+	// paste to stdout
+	data2, _ := clip.ReadAll(clipper.RegClipboard)
+	fmt.Print(string(data2))
+}
+
+/*
+	func Example() {
+		//clipboard.WriteAll("text")
+		selected =
+		text, _ := clipboard.ReadAll()
+		fmt.Println(text)
+
+		// Output:
+		// 日本語
+	}
+*/
